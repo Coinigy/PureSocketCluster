e72ea27aef014c73576b3b6e192d417cf1a584e3
Allen Byron Penner <byronap@coinigy.com>
2018-04-27 12:49:59 -0400

    add our default serializer (UTF8JSON)

---
+++ src/PureSocketCluster/UTF8JSONSerializer.cs
@@ -0,0 +1,11 @@
+﻿using Utf8Json;
+
+namespace PureSocketCluster
+{
+    internal class Utf8JsonSerializer : ISerializer
+    {
+	    public T Deserialize<T>(string json) => JsonSerializer.Deserialize<T>(json);
+
+	    public byte[] Serialize(object obj) => JsonSerializer.Serialize(obj);
+    }
+}
